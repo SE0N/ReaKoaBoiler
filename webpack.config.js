f2aa0397b54d2b91ad971b59bad4b58e581dd7de
Seonnyn <admin@seonnyn.de>
2016-03-16 17:08:08 +0000

    Webpack move to babel core

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: 'babel'
+            loader: 'babel-core'
         }]
     },
     resolve: {
