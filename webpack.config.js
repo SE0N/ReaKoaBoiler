e235671d3f7e1410fc35ac3128b38bfde4631376
Seonnyn <admin@seonnyn.de>
2016-03-16 16:07:50 +0000

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+/*eslint-env node */
+module.exports = {
+    entry: [
+        "./app/src/index.js"
+    ],
+    output: {
+        path: __dirname + '/app/build',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './app/build'
+    }
+};
